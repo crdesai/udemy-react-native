50cc00b4abe6156ab5853ea1719b5bd22cdf7561
Chintan Desai <cd.chintan@gmail.com>
2015-12-27 00:20:35 -0800

    dynamic style and weekdays list

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -1,35 +1,30 @@
 // Import the libraries we need
 var React = require('react-native');
+var Moment = require('moment');
 var AppRegistry = React.AppRegistry;
 var View = React.View;
 var Text = React.Text;
 var StyleSheet = React.StyleSheet;
 var DayItem = require('./src/day-item');
 
-var DAYS = [
-	'Sunday',
-	'Monday',
-	'Tuesday',
-	'Wednesday',
-	'Thursday',
-	'Friday',
-	'Saturday'
-];
 
 // Create a React component
 var Weekdays = React.createClass({
   render: function() {
   	return <View style={styles.container}>
-  		<Text>
-  			Days of the week:
-  		</Text>
+  		
   		{this.renderDay()}
   	</View>
   },
   renderDay: function() {
-  	return DAYS.map(function(day) {
-  		return <DayItem day={day} key={day}/>
-  	});
+  	var dayItems = [];
+  	for(var i=0; i<7; i++) {
+  		var day = Moment().add(i, 'days').format('dddd');
+  		dayItems.push([
+  			<DayItem day={day} key={day} daysUntil={i} />
+  		]);
+  	}
+  	return dayItems;
   }
 });
 
--- weekdays/src/day-item.js
+++ weekdays/src/day-item.js
@@ -5,16 +5,31 @@ var StyleSheet = React.StyleSheet;
 
 var DayItem = React.createClass({
 	render: function() {
-		return <Text style={styles.day}>
+		return <Text style={this.generateStyles()}>
 			{this.props.day}
 		</Text>
-	}
-});
-
-var styles = StyleSheet.create({
-	day: {
-		color: '#0000ff',
-		fontSize: 18
+	},
+	generateStyles: function(){
+		return {
+			color: this.color(),
+			fontWeight: this.fontWeight(),
+			fontSize: this.fontSize(),
+			lineHeight: this.lineHeight()
+		}
+	},
+	color: function(){
+		var opacity = 1 / this.props.daysUntil;
+		return 'rgba(0,0,255, ' + opacity + ')';
+	},
+	fontWeight: function(){
+		var weight = 7 - this.props.daysUntil;
+		return (weight * 100).toString();
+	},
+	fontSize: function() {
+		return 60 - 6* this.props.daysUntil;
+	},
+	lineHeight: function(){
+		return 70 - 4 * this.props.daysUntil;
 	}
 });
 
